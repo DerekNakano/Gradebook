94c6f9d9bdc8e17389ecbd25c8fa1f78ebe2970f
Derek Nakano <9661730+DerekNakano@users.noreply.github.com>
2018-03-22 15:43:03 -0700

    cleaned up a bit

--- src/GradeBook.java
+++ src/GradeBook.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.sql.*;
-//import com.microsoft.sqlserver.jdbc.*;
+
 
 public class GradeBook {
 
@@ -30,112 +30,17 @@ public class GradeBook {
 				connection = DriverManager.getConnection(connectionString);
 				System.out.println("Connection successful!");
 				
-				//ArrayList<Student> students = new ArrayList<Student>();
-				
-				Statement statement;
-				ResultSet resultSet;
-				
 				helpMessage();
+				boolean cont = true;
 				
-				while(input.hasNextLine())	//keeps looping while inputting in scanner
+				while(cont)	//reads input and performs the appropriate operation
 				{
-					String operation = input.next();
+					input.hasNextLine();
+					String op = input.next();
 					
-					String selectSql = ""; 
-					
-					if(operation.equals("done"))		//break out of loop if user just inputs "done"
-						break;
-					else if (operation.equals("help"))	//outputs the help message
-						helpMessage();
-					else if (operation.equals("display"))
-					{
-						String table = input.next();
-						
-						if(table.equals("student"))
-						{
-							selectSql = "SELECT * from Student;";
-							
-							statement = connection.createStatement();  
-			                resultSet = statement.executeQuery(selectSql);
-			                
-			                while(resultSet.next())
-			                {
-			                	System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " "  
-			                        + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5));
-			                }
-						}
-						else if (table.equals("test"))
-						{
-							selectSql = "SELECT * from Test;";
-							
-							statement = connection.createStatement();  
-			                resultSet = statement.executeQuery(selectSql); 
-			                
-			                while(resultSet.next())
-			                {
-			                	System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " "  
-			                        + resultSet.getString(3) + " " + resultSet.getString(4) + " "); 
-			                }
-						}
-					}
-					else if (operation.equals("add"))	//adds a student to database
-					{
-						String sID = input.next(), first= input.next(), last= input.next(), year= input.next(); 
-						selectSql = "insert into Student values (" + sID + ",'" + first + "','" + last + "'," + year + ",NULL);";
-						
-						statement = connection.createStatement();  
-		                int updated = statement.executeUpdate(selectSql); 
-		                
-		                System.out.println(updated + " row(s) were affected.");
-					}
-					else if (operation.equals("remove"))	//removes a student to database
-					{
-						String item = input.next();
-						
-						if(item.equals("student"))
-						{
-							String sID = input.next(); 
-							selectSql = "delete from Student where " + sID + "=sID;";
-						}
-						else if (item.equals("test"))
-						{
-							String tID = input.next(), sID = input.next(); 
-							selectSql = "delete from Test where " + sID + "=sID and " + tID + "=tID;";
-						}
-						statement = connection.createStatement();  
-
-
-						int updated = statement.executeUpdate(selectSql); 		                
-		                System.out.println(updated + " row(s) were affected.");
-					}
-					else if (operation.equals("test"))	//adds a test to database
-					{
-						String tID = input.next(), sID = input.next(), score = input.next(), overall = input.next(); 
-						selectSql = "insert into Test values (" + tID + "," + sID + "," + score + "," + overall + ");";
-						
-						statement = connection.createStatement();  
-
-						int updated = statement.executeUpdate(selectSql); 		                
-		                System.out.println(updated + " row(s) were affected.");
-					}
-					else if (operation.equals("calc"))	//calculates all the students' grades and outputs them
-					{
-						
-						String subq = "(SELECT sID, sum(Score) as Score, sum(Overall) as Overall from Test group by sID) as G";
-						selectSql = "SELECT S.sID, S.First, S.Last, S.Year, FORMAT (100.0*Score/Overall,'0.00') from Student S, " + subq + " where S.sID = G.sID;";
-						
-						
-						statement = connection.createStatement();  
-		                resultSet = statement.executeQuery(selectSql);
-		                
-		                // Print results from select statement  
-		                while (resultSet.next())   
-		                {  
-		                    System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " "  
-		                        + resultSet.getString(3) + " " + resultSet.getString(4) + "  %" + resultSet.getString(5));  
-		                } 
-					}
+					cont = performOperation(connection, op, input);
 				}
+				
 				input.close();
 			}
 			catch (Exception e) {
@@ -161,28 +66,107 @@ public class GradeBook {
 				+ "[help] to if you want this message to be repeated");
 	}
 	
-	/*
-	public static int checkList(ArrayList<Student> roster, String name)		//returns index of student on ArrayList, if no student then returns -1
+	
+	public static boolean performOperation(Connection connection, String operation, Scanner input) throws SQLException
 	{
-		for(int i = 0; i < roster.size(); i++)
+		Statement statement;
+		ResultSet resultSet;
+		String selectSql = ""; 
+		
+		if(operation.equals("done"))		//break out of loop if user just inputs "done"
+			return false;
+		else if (operation.equals("help"))	//outputs the help message
+			helpMessage();
+		else if (operation.equals("display"))
 		{
-			if(roster.get(i).getName().equals(name))
-				return i;
+			String table = input.next();
+			
+			if(table.equals("student"))
+			{
+				selectSql = "SELECT * from Student;";
+				
+				statement = connection.createStatement();  
+                resultSet = statement.executeQuery(selectSql);
+                
+                while(resultSet.next())
+                {
+                	System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " "  
+                        + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5));
+                }
+			}
+			else if (table.equals("test"))
+			{
+				selectSql = "SELECT * from Test;";
+				
+				statement = connection.createStatement();  
+                resultSet = statement.executeQuery(selectSql); 
+                
+                while(resultSet.next())
+                {
+                	System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " "  
+                        + resultSet.getString(3) + " " + resultSet.getString(4) + " "); 
+                }
+			}
 		}
-		return -1;
-	}
-	
-	public static void outputGrade(ArrayList<Student> roster)
-	{
-		for(int i = 0; i < roster.size(); i++)
-			System.out.printf("%s current overall grade is %.2f\n", roster.get(i).getName(), roster.get(i).getGrade());
-	}
+		else if (operation.equals("add"))	//adds a student to database
+		{
+			String sID = input.next(), first= input.next(), last= input.next(), year= input.next(); 
+			selectSql = "insert into Student values (" + sID + ",'" + first + "','" + last + "'," + year + ",NULL);";
+			
+			statement = connection.createStatement();  
+            int updated = statement.executeUpdate(selectSql); 
+            
+            System.out.println(updated + " row(s) were affected.");
+		}
+		else if (operation.equals("remove"))	//removes a student to database
+		{
+			String item = input.next();
+			
+			if(item.equals("student"))
+			{
+				String sID = input.next(); 
+				selectSql = "delete from Student where " + sID + "=sID;";
+			}
+			else if (item.equals("test"))
+			{
+				String tID = input.next(), sID = input.next(); 
+				selectSql = "delete from Test where " + sID + "=sID and " + tID + "=tID;";
+			}
+			statement = connection.createStatement();  
 
-	
-	public static void outputGrade(ArrayList<Student> roster, String name)
-	{
-		int index = checkList(roster, name);
-		System.out.printf("%s current overall grade is %.2f\n", roster.get(index).getName(), roster.get(index).getGrade());
+
+			int updated = statement.executeUpdate(selectSql); 		                
+            System.out.println(updated + " row(s) were affected.");
+		}
+		else if (operation.equals("test"))	//adds a test to database
+		{
+			String tID = input.next(), sID = input.next(), score = input.next(), overall = input.next(); 
+			selectSql = "insert into Test values (" + tID + "," + sID + "," + score + "," + overall + ");";
+			
+			statement = connection.createStatement();  
+
+			int updated = statement.executeUpdate(selectSql); 		                
+            System.out.println(updated + " row(s) were affected.");
+		}
+		else if (operation.equals("calc"))	//calculates all the students' grades and outputs them
+		{
+			
+			String subq = "(SELECT sID, sum(Score) as Score, sum(Overall) as Overall from Test group by sID) as G";
+			selectSql = "SELECT S.sID, S.First, S.Last, S.Year, FORMAT (100.0*Score/Overall,'0.00') from Student S, " + subq + " where S.sID = G.sID;";
+			
+			
+			statement = connection.createStatement();  
+            resultSet = statement.executeQuery(selectSql);
+            
+            // Print results from select statement  
+            while (resultSet.next())   
+            {  
+                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " "  
+                    + resultSet.getString(3) + " " + resultSet.getString(4) + "  %" + resultSet.getString(5));  
+            } 
+		}
+		
+		return true;
 	}
-	*/
+	
 }
